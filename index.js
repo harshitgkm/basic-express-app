0aadb4ec20458a5b5e84e24237ab8fe61c370273
harshit <harshitpurohit@gkmit.co>
2024-10-09 16:41:19 +0530

    docs/express app for heroku

---
+++ index.js
@@ -0,0 +1,15 @@
+	const express = require("express");
+
+	const app = express();
+
+	const port = 3000;
+
+
+	app.get("/health", (req,res)=>{
+		res.send("Testing!");
+	})
+
+	app.listen(port, ()=> {
+		console.log(`Listenning to port no. ${port}`);
+
+	})
